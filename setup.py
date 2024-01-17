8d3a91c6f06c64b98eb7473d1e5d030a361f3d99
Hidetaka Takano <hidetaka.takano@toshiba.co.jp>
2024-01-17 19:32:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 setup(
     name="cmakelang-precommit-dummy",
     version="0.0.0",
-    install_requires=["cmakelang==0.6.13"]
+    install_requires=[
+        "cmakelang==0.6.13",
+        "pyyaml"
+    ]
 )
